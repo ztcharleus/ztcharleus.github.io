cf1a932b45199185600384debaba5a5e84b3adc3
Alex Leitch <aleitch1@umd.edu>
2020-07-17 14:34:47 -0400

    Chain connected properly

--- lab_utils/html_validation_error_gen.js
+++ lab_utils/html_validation_error_gen.js
@@ -1,13 +1,13 @@
 "use strict";
 
-import { spawnSync } from "child_process";
+import { spawnSync, spawn } from "child_process";
 import util from "util";
 import fs from "fs";
 import path from "path";
 import process from "process";
 import recursiveReaddir from "recursive-readdir";
 
-// const readdir = util.promisify(recursiveReaddir);
+const writeFile = util.promisify(fs.writeFile);
 
 console.log(process.argv);
 // Get the file name we're working on
@@ -19,61 +19,51 @@ console.log(process.argv);
 // Then we're going to get that as a folder name?
 // then we'll loop the public folder and process all HTML documents we find
 // we'll need a delimiter to block more than 15 found documents
-function ignoreThisStuff(file, stats) {
-    return (path.extname(file).length && path.extname(file) !== ".html");
+function excludeNonHTML(file, stats) {
+  return path.extname(file).length && path.extname(file) !== ".html";
 }
 
-recursiveReaddir("./public", [ignoreThisStuff, ".DS_Store"])
+// TODO: comment this in before export
+function excludeAnswerKey(file, stats) {
+  // return stats.isDirectory() && path.basename(file) == "answer_key";
+}
+
+async function processHTML(fileName) {
+  // fire HTML-Validate
+  const ls = await spawn("npx", ["html-validate", `./${fileName}`]);
+  
+  // get the string from the process
+  const newString = ls.stdout.toString();
+
+  // String processing
+  const nS1 = newString.split("\n");
+  const d = nS1.filter((f) => f.match(/\d+:\d+/g));
+  const prune = d.map((m) => m.trim());
+  console.log(prune);
+
+  // Write the file
+  return writeFile( `./cypress/fixtures/tempFileName.json`, JSON.stringify(prune))
+    .then((err) => {
+        return "Success"
+    });
+}
+
+recursiveReaddir("./public", [excludeNonHTML, excludeAnswerKey, ".DS_Store"])
   .then((data) => {
     console.log("readDir data", data);
+    return data;
   })
-  .then(() => {
-    const tempFileName = "lab_2_html.json";
-    const ls = spawnSync("npx", [
-      "html-validate",
-      "../public/answer_key/lab_2/index.html",
-    ]);
+  .then((data) => {
+    // for each file in data, we're going to want to run the validator and output an updated file.
+    // this needs to happen asynchronously because each process is its own thing
+
+    return processHTML('public/answer_key/lab_2/index.html');
 
     // todo
     // write file
-    // generalize index to test processing
-    // list available filenames
     // write them to a cypress fixture
     // put all string values re labs into a config file
-
-    const newString = ls.stdout.toString();
-    // console.log(newString);
-
-    const nS1 = newString.split("\n");
-
-    const d = nS1.filter((f) => f.match(/\d+:\d+/g));
-    const prune = d.map((m) => m.trim());
-    console.log(prune);
-
-    fs.writeFile(
-      `../cypress/fixtures/${tempFileName}`,
-      JSON.stringify(prune),
-      () => {
-        console.log("HTML validation array file write success");
-      }
-    );
-
-    // const processedResults = nS1.map(m => m.substring(m.indexOf('error')));
-    // console.log(processedResults);
-    // const splitResults = result.stdout.match(/\[\d+merror.+/g);
-    // const cleanResults = splitResults.map((r) => r.replace(/\[\d+m/g, ""));
-    // const errors = results.match(/error/g);
-
-    // Logging to check values for debug
-    // cy.log(cleanResults.length);
-    // cleanResults.forEach(e => {
-    //   cy.log(e);
-    // })
-
-    // const blob = cleanResults.reduce((c, i) => {
-    //   if (i.includes("error")) {
-    //     c.push(i);
-    //   }
-    //   return c;
-    // }, []);
+  })
+  .then((data) => {
+    console.log(data);
   });
