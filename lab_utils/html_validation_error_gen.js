e0cc0ce025cfdec454fa0f593f0d35c92c483075
Alex Leitch <aleitch1@umd.edu>
2020-07-20 19:13:01 -0400

    Update html_validation_error_gen.js
    - Functions moved to function library
    - Errors output properly on the command line
    - Now working on CLI messaging

--- lab_utils/html_validation_error_gen.js
+++ lab_utils/html_validation_error_gen.js
@@ -1,14 +1,19 @@
 "use strict";
+// import util from "util";
+// import fs from "fs";
 
-import { exec } from "child_process";
-import util from "util";
-import fs from "fs";
-import path from "path";
 import recursiveReaddir from "recursive-readdir";
-import chalk from 'chalk';
+import chalk from "chalk";
 
-const writeFile = util.promisify(fs.writeFile);
-const execPr = util.promisify(exec);
+import {
+  processHTML,
+  excludeNonHTML,
+  excludeAnswerKey,
+  cleanTerminalOutput,
+} from "./functions.js";
+
+// const writeFile = util.promisify(fs.writeFile);
+
 
 // Get the file name we're working on
 // I want to walk a folder for things to validate
@@ -19,76 +24,30 @@ const execPr = util.promisify(exec);
 // Then we're going to get that as a folder name?
 // then we'll loop the public folder and process all HTML documents we find
 // we'll need a delimiter to block more than 15 found documents
-function excludeNonHTML(file, stats) {
-  return path.extname(file).length && path.extname(file) !== ".html";
+function partition(array, isValid) {
+  return array.reduce(([pass, fail], elem) => {
+    return isValid(elem) ? [[...pass, elem], fail] : [pass, [...fail, elem]];
+  }, [[], []]);
 }
 
-// TODO: comment this in before export
-function excludeAnswerKey(file, stats) {
-  // return stats.isDirectory() && path.basename(file) == "answer_key";
-}
 
-function cleanTerminalOutput(string) {
-  const nS1 = string.split("\n");
-  const d = nS1.filter((f) => f.match(/\d+:\d+/g));
-  return d.map((m) => m.trim());
-}
+function severeErrorTest(f) {return f["Error Type"] === "element-required-content"}
 
-function headAndBodyCheck(str) {
-  return str.includes("element-required-content");
-}
+function severeErrorCheck(document) {
+    // console.log(document);
 
-function severeErrorCheck(arr) {
   // do some warnings in case some errors are severe
-  arr.forEach((d) => {
-    const errors = d.errorCollection.filter((f) =>
-      f.includes("element-required-content")
-    );
-    if (errors.length > 0) {
-      console.log(
-        chalk.red(`You have serious structural problems in your HTML for ${chalk.bold(d.title)}.`)
-      );
-      console.log("Please correct them before attempting the lab problems.");
-      const messages = errors.map((err) =>
-        err
-          .split("  ")
-          .map(m => m.trim())
-          .filter((f) => f.length > 0 && f !== 'error')
-          .reduce((acc, curr, idx) => {
-            const idxChr = {
-              0: "Line number",
-              1: "Error",
-              2: "Error Type"
-            };
-            acc[idxChr[idx]] = curr
-            return acc;
-          }, {})
-      );
-      console.table(messages);
-    }
-  });
-
-  return arr;
+    const splitArray = partition(document.errorCollection, severeErrorTest);
+    // console.log("BAD BUGS", splitArray[0]);
+    // console.log("OKAY BUGS", splitArray[1]);
+    return {
+      ...document,
+      errorCollection: splitArray[1],
+      severe: splitArray[0],
+    };
 }
 
 const stringOutput = {};
-
-async function processHTML(filename) {
-  // fire HTML-Validate process
-  const ls = await execPr(`npx html-validate "./${filename}"`).catch((err) => {
-    // NB: for whatever reason, .exec sees NPX as failing? This error is in Node itself.
-    if (err.stdout) {
-      return err.stdout;
-    }
-  });
-  // get the string from the process
-  const terminalOutput = ls.toString();
-  const errorCollection = cleanTerminalOutput(terminalOutput);
-  return {
-    filename,
-    errorCollection,
-  };
-}
 
 recursiveReaddir("./public", [excludeNonHTML, excludeAnswerKey, ".DS_Store"])
   .then((data) => {
@@ -103,24 +62,52 @@ recursiveReaddir("./public", [excludeNonHTML, excludeAnswerKey, ".DS_Store"])
       return m;
     })
   )
-  .then((data) => severeErrorCheck(data))
+  .then((data) => data.map(m => severeErrorCheck(m)))
   .then((data) => {
-    return Promise.all(
-      data.map((m) => {
-        return writeFile(
-          `./cypress/fixtures/generated/${m.title}.json`,
-          JSON.stringify(m.errorCollection)
-        ).catch((error) => console.log("write error", err));
-      })
-    );
-  })
-  .then((data) => {
-    console.log("Preliminary HTML validation check complete");
-    if (data.length > 0) {
-      const str = data.length === 1 ? "document has" : "documents have";
-      console.log(data.length, `${str} errors`);
+
+    // DO YOUR OUTPUTS
+    console.log(chalk.bgCyan.bold("Preliminary HTML validation check complete"));
+    // if any document has severe errors, list it and put it in a table
+    // and block cypress launch
+    let blockCypress;
+    data.forEach(d => {
+      if (d.severe.length > 0) {
+        console.table(d.severe);
+        console.error(
+          `Please repair your HTML in ${d.title} before proceeding with the lab.`
+        );
+        blockCypress === true;
+      } else if (d.errorCollection.length > 0) {
+        const str = data.length === 1 ? "document has" : "documents have";
+        console.log(chalk.yellow(data.length, `${str} errors`));
+        console.log("You can fix your validation errors in the test suite.");
+      }
+    });
+
+
+    // Else tabulate the errors we have
+  
+    // Else green to go and good luck with lab problems.
+
+    if (data.severeErrors.length > -1) {
+      console.table(data.severeErrors);
+      console.error(
+        "Please repair your basic HTML before proceeding with the lab."
+      );
     } else {
-      console.log("No invalid HTML detected");
+      console.log(
+        chalk.green.bold("No invalid HTML detected! Good luck with your lab.")
+      );
     }
+    // we may not need to write these files if we're doing Head checks before
+    // we allow Cypress to open at all.
+    // return Promise.all(
+    //   data.map((m) => {
+    //     return writeFile(
+    //       `./cypress/fixtures/generated/${m.title}.json`,
+    //       JSON.stringify(m.errorCollection)
+    //     ).catch((error) => console.log("write error", err));
+    //   })
+    // );
   })
   .catch((err) => console.log(err));
