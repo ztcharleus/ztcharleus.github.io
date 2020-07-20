6ffd3751937a22a1a3391472d75cce6a110a0ca0
Alex Leitch <aleitch1@umd.edu>
2020-07-20 17:01:08 -0400

    Add custom logging command but you can't turn it red

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,4 +23,19 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
-import 'cypress-wait-until';
+
+Cypress.Commands.add("logBasicHTMLErrors", (key, value) => {
+  const log = Cypress.log({
+    name: "sampleHTMLError",
+    displayName: "HTML Validation Error",
+    message: `${key}, ${value}`,
+    consoleProps: () => {
+      return {
+        Key: key,
+        Value: value,
+      };
+    },
+  });
+});
+
+import "cypress-wait-until";
